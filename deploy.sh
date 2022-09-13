eae2d9b40b97e559cacc46ddddf1aef7177225cb
Chaithra B R <112927222+chaithrabr02@users.noreply.github.com>
2022-09-13 15:28:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ Updating thr master branch
 
 We are in dev branch
 This file contain some deplyment code
-This the new line I am addeing to the stage banch
+This the new line I am addeing to the stage banchh
